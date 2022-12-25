040b0dd752d66a7c18e92aef35bd58558b19fd9d
gouiferda <soufiane.gouiferda@gmail.com>
2022-12-25 15:49:23 +0100

    ok

---
+++ README.md
@@ -0,0 +1,22 @@
+# MAC UPDATER
+
+
+Apps: 
+iterm
+Keka
+VLC
+Chrome
+Firefox
+Appcleaner
+HomeBrew
+zsh
+ohmyzsh
+Qbitorrent
+Java
+Latest
+Maccy
+Rectangle
+
+
+Other services :
+macapps.link
---
+++ start.sh
@@ -0,0 +1,10 @@
+
+
+xcode-select —-install
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install java google-chrome firefox qbittorrent appcleaner latest iterm2 zsh maccy rectangle vlc keka iterm2 wget 
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
